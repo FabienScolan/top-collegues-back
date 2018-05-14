1460b42ec2b5deead0ed92435e5877b772c6e5c5
Fabien SCOLAN <fabien.scolan22@gmail.com>
2018-05-14 16:06:37 +0200

    Essai methode PATCH

--- src/main/java/dev/top/controller/CollegueCtrl.java
+++ src/main/java/dev/top/controller/CollegueCtrl.java
@@ -1,12 +1,19 @@
 package dev.top.controller;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.top.entities.Action;
 import dev.top.entities.Collegue;
 import dev.top.repos.CollegueRepo;
 
@@ -19,6 +26,29 @@ public class CollegueCtrl {
 	@GetMapping
 	public List<Collegue> findAll() {
 		return this.collegueRepo.findAll();
+	}
+
+	@RequestMapping(value = "/collegues/{pseudo}", method = RequestMethod.PATCH)
+	public ResponseEntity<?> patchCollegue(@PathVariable("pseudo") String pseudo, @RequestBody Action action) {
+		// logger.info("Fetching User with id {}", id);
+		Collegue collegue = null;
+		List<Collegue> listeCollegues = collegueRepo.findAll();
+		Iterator<Collegue> it = listeCollegues.iterator();
+		while (it.hasNext()) {
+			Collegue comp = it.next();
+			if (comp.getPseudo().equals(pseudo))
+				collegue = comp;
+		}
+		if (collegue == null) {
+			// logger.error("User with id {} not found.", id);
+			return new ResponseEntity("User with pseudo " + pseudo + " not found", HttpStatus.NOT_FOUND);
+		}
+		if (action == Action.AIMER) {
+			collegue.setScore(collegue.getScore() + 100);
+		} else if (action == Action.DETESTER) {
+			collegue.setScore(collegue.getScore() - 50);
+		}
+		return new ResponseEntity<Collegue>(collegue, HttpStatus.OK);
 	}
 
 }
